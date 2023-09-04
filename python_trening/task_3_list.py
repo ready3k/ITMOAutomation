77767fafb2bf7ed94f8c2979c79ef2319e87d80f
Roman_Strokov <ready3k@gmail.com>
2023-09-04 20:35:56 +0300

    2nd Commit

---
+++ python_trening/task_3_list.py
@@ -0,0 +1,19 @@
+a = [5, 10, 15, 29, 25, 30, 35, 40]
+
+print("a[2] = ", a[2])
+
+print("a[0:3] = ", a[0:3])
+
+b = [11, 12, 13]
+b[2] = 4
+print(b)
+
+test_list = ['one', 'two', 'three', 'four', 'five']
+
+for elem in test_list:
+    print(elem)
+
+print(len(test_list))
+
+test_list.append('six')
+print(test_list)
---
+++ python_trening/task_8_if.py
@@ -0,0 +1,17 @@
+num = -3
+
+
+if num >= 0:
+    print("Число больше или равно 0")
+else:
+    print("число отрицательное")
+
+
+str_1 = 'test'
+str_2 = 'test1'
+
+
+if str_1 in str_2:
+    print('ДА')
+else:
+    print('НЕТ')
